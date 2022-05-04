c446a1b95313a0456d1ba7d86f0833b59445a25b
Keschin <62339586+Keschin@users.noreply.github.com>
2022-05-04 17:14:51 +0200

    Corrected some imports and functions

--- API.py
+++ API.py
@@ -1,5 +1,6 @@
+import json
 import spotipy
-from spotipy import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth
 
 # Hier werden die Rechte und persönliche ID's vergeben
 client_ID = 'ba90c35f938e41b293136e6e325c699d'
@@ -10,8 +11,8 @@ client_redirect_uri = 'http://localhost:99/callback'
 # Hier nutzen wir die erstellten Reche und ID's für eine Authentifikation damit wir einen Token erhalten.
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id = client_ID, client_secret = client_Secret, redirect_uri = client_redirect_uri))
 
-# Test Commit für Github Desktop Version
-results = sp.current_user_saved_tracks(limit=50, offset=0, time_range='medium_term')
+# Hier werden die TopTracks gesucht mit bestimmten Parametern. Limit wird auf 50 gesetzt für die Top 50
+results = sp.current_user_top_tracks(limit=50, offset=0, time_range='medium_term')
 for song in range (50):
     list = []
     list.append(results)
@@ -56,3 +57,5 @@ for song in range (50):
              'popularity': list_of_popularity
             })
         all_songs_saved = all_songs.to_csv('top50_songs.csv')
+
+
